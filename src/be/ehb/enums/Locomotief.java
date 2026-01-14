f292c900b578bfd73363cf56c806eafff6e70351
Vippy11-team <Walouma@hotmail.com>
2026-01-14 18:18:14 +0100

    Voeg enums voor Locomotief en Personeelsrol toe

---
+++ src/be/ehb/enums/Locomotief.java
@@ -0,0 +1,25 @@
+package be.ehb.enums;
+
+
+/**
+ * Type locomotief dat bepaalt hoeveel wagons een trein maximaal kan hebben
+ */
+
+public enum Locomotief {
+
+
+
+    C373(12),
+    C374(14);
+
+    private final int maxAantalWagons;
+
+
+    Locomotief(int maxAantalWagons) {
+        this.maxAantalWagons = maxAantalWagons;
+    }
+
+    public int getMaxAantalWagons() {
+        return maxAantalWagons;
+    }
+}
---
+++ src/be/ehb/enums/Personeelsrol.java
@@ -0,0 +1,14 @@
+package be.ehb.enums;
+
+/**
+ * De rollen die een Personeelslid kan hebben
+ */
+
+public enum Personeelsrol {
+
+    BESTUURDER,
+    STEWARD,
+    BAGAGIST
+
+
+}
