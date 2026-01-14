6a72a14d5d224e7372aac78469101ebf23bad627
Vippy11-team <Walouma@hotmail.com>
2026-01-14 18:31:04 +0100

    Voeg abstracte klasse Persoon toe als basis voor Passagier en Personeelslid

---
+++ src/be/ehb/klassen/Persoon.java
@@ -0,0 +1,52 @@
+package be.ehb.klassen;
+
+import java.time.LocalDate;
+
+/**
+ * Abstracte klasse voor een persoon
+ * Wordt gebruikt als basis voor passagiers en personeelsleden
+ */
+
+public abstract class Persoon {
+
+    private final String naam, achternaam, rijkregisternummer;
+    private final LocalDate geboortedatum;
+
+    public Persoon(String naam, String achternaam, String rijkregisternummer, LocalDate geboortedatum) {
+        this.naam = naam;
+        this.achternaam = achternaam;
+        this.rijkregisternummer = rijkregisternummer;
+        this.geboortedatum = geboortedatum;
+    }
+
+
+    public String getRijkregisternummer() {
+        return rijkregisternummer;
+    }
+
+    public LocalDate getGeboortedatum() {
+        return geboortedatum;
+    }
+
+
+    public String getNaam() {
+        return naam;
+    }
+
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    @Override
+    public String toString() {
+        return "Persoon{" +
+                "naam='" + naam + '\'' +
+                ", achternaam='" + achternaam + '\'' +
+                ", rijkregisternummer='" + rijkregisternummer + '\'' +
+                ", geboortedatum=" + geboortedatum +
+                '}';
+    }
+
+
+}
