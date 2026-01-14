892678561fa9f651f1c9c31dfe459e440de275ec
Vippy11-team <Walouma@hotmail.com>
2026-01-14 18:05:58 +0100

    Voeg Station en Klasse enums voor de projectstructuur

---
+++ src/be/ehb/enums/Klasse.java
@@ -0,0 +1,14 @@
+package be.ehb.enums;
+
+/**
+ * Klasse van een wagon of ticket
+ * Wordt gebruikt als ticketinformatie en om de capaciteit per klasse te berekenen
+ */
+
+public enum Klasse {
+    EERSTE,
+    TWEEDE
+
+
+
+}
---
+++ src/be/ehb/enums/Station.java
@@ -0,0 +1,16 @@
+package be.ehb.enums;
+
+/**
+ * Alle mogelijke stations voor een reis
+ */
+
+public enum Station {
+
+    BRUSSEL,
+    PARIJS,
+    AMSTERDAM,
+    BERLIJN,
+    PRAGUE,
+    MADRID,
+    ROME
+}
