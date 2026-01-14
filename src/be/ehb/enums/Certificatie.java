b8950b6cf7be6475163dd2c4fb9aebaf00f7f204
Vippy11-team <Walouma@hotmail.com>
2026-01-14 18:22:25 +0100

    Voeg enum Certificatie toe

---
+++ src/be/ehb/enums/Certificatie.java
@@ -0,0 +1,16 @@
+package be.ehb.enums;
+
+/**
+ * Certificaties die een personeelslid kan hebben
+ * Wordt gebruikt om de vaardigheden/certificaten bij te houden
+ */
+
+public enum Certificatie {
+    VEILIGHEID,
+    RIJBEWIJS_B1,
+    RIJBEWIJS_A3,
+    TOERISME
+
+
+
+}
